58f4c5fd92bd42f233e8508015d2d7fd1161e4a5
Armand B <71330618+ArmandBroutinDev@users.noreply.github.com>
2020-11-30 12:30:29 +0100

    Update app.py
    fin du quickstart avec template

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import escape
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -7,11 +8,13 @@ titre = '<title>HELLO H3</title>'
 
 @app.route("/")
 def index():
-    return titre + "accueil"
+    #return titre + "accueil"
+    return render_template('hello.html')
     
 @app.route("/hello/<string:username>")
 def salutation(username):
-    return "Hello %s" % escape(username)
+	return render_template('hello.html',name = username)
+    #return "Hello %s" % escape(username)
 
 if __name__ == '__main__':
     app.run(debug=True)
