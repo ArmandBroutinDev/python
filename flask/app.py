dea1efd5a7a3d5a63b6c6c700dc75b1ef72d937b
Armand B <71330618+ArmandBroutinDev@users.noreply.github.com>
2020-11-30 12:08:20 +0100

    Update app.py
    quickstart ajout du hello

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
-from flask import Response
+from flask import escape
 
 app = Flask(__name__)
 
+titre = '<title>HELLO H3</title>'
+
 @app.route("/")
 def index():
-    return '<title>HELLO H3</title>'
-
+    return titre + "accueil"
+    
+@app.route("/hello/<string:username>")
+def salutation(username):
+    return "Hello %s" % escape(username)
 
 if __name__ == '__main__':
     app.run(debug=True)
