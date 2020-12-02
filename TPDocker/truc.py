0b82d3998a24d368eb5509a03b2aedb0d70c4c33
Armand B <71330618+ArmandBroutinDev@users.noreply.github.com>
2020-12-02 14:46:25 +0100

    Update truc.py

--- TPDocker/truc.py
+++ TPDocker/truc.py
@@ -17,4 +17,4 @@ def salutation(username):
     #return "Hello %s" % escape(username)
 
 if __name__ == '__main__':
-    app.run(debug=True,host="localhost",port=5000)
+    app.run(debug=True,host="0.0.0.0",port=5000)
